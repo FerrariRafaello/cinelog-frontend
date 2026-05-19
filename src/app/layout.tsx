5bf7de94f06549e95a1a657a1f29615abfff4725
FerrariRafaello <rafaellosferrari@gmail.com>
2026-05-19 15:01:28 -0300

    fix dns path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const viewport={
 };
 
 export const metadata: Metadata = {
-  title: "Cinelog",
+  title: "CritCine",
   description: "Movie rating platform",
   icons: {
     icon: "/icon.svg",
