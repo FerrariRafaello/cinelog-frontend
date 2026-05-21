b9163bcf0423810a79521fa9a55a281d97e889ca
FerrariRafaello <rafaellosferrari@gmail.com>
2026-05-21 04:22:43 -0300

    fix login page footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
-import { Footer } from "@/components/Footer";
 
 
 const geistSans = Geist({
@@ -39,7 +38,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        <Footer />
         <Toaster />
     </body>
     </html>
